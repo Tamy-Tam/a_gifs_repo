47a5501152eafdc1cca5a9a87a4133cb2471ce05
Tamara Robson <tamara.jade.robson@gmail.com>
2021-04-24 22:56:01 +0200

    Started making the search bar (functionnal) but not displaying

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,6 +4,21 @@ import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    }
+  }
+
+
+  search = (query) => {
+    // TODO: API Call
+  }
+
+
   render() {
     const gifs = [
       { id: 'xT9IgDEI1iZyb2wqo8' },
@@ -15,11 +30,11 @@ class App extends Component {
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="xT9IgDEI1iZyb2wqo8" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
